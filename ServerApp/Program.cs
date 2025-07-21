1fe92b98694df7041a996260c719b0db68448331
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-07-21 16:28:47 +0200

    add serverapp

---
+++ ServerApp/Program.cs
@@ -0,0 +1,39 @@
+using ServerShared;
+using ServerShared.Controllers;
+using Shared;
+
+namespace ServerApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MainLogger.CreateNew();
+        if (args.Contains("clean"))
+        {
+            // Deleting all files and the database too.
+            var log_files = Directory.GetFiles(Environment.CurrentDirectory, "*.log", SearchOption.AllDirectories);
+            foreach (var logfile in log_files)
+                File.Delete(logfile);
+            Directory.Delete("Database", true);
+        }
+        if (args.Contains("debug"))
+        {
+            MainLogger.LevelSwitch.MinimumLevel = LogEventLevel.Debug;
+        }
+
+        PluginController.LoadPlugins();
+        string endCheck = "not";
+        while (!endCheck.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+        {
+            endCheck = Console.ReadLine()!;
+            if (endCheck.StartsWith('!'))
+            {
+                CommandController.Run(endCheck);
+            }
+        }
+        PluginController.UnloadPlugins();
+        Console.ReadLine();
+    }
+
+}
