c5fd76b40f44735a2689509e308d40d0eeb2b9d2
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-07-24 23:10:29 +0200

    add more stuff

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,6 +1,12 @@
+using ModdableWebServer.Helper;
+using NetCoreServer;
+using Serilog;
+using Serilog.Events;
+using ServerShared.CommonModels;
 using ServerShared.Controllers;
 using Shared;
-using Serilog.Events;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ServerApp;
 
@@ -8,6 +14,32 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        if (args.Contains("example"))
+        {
+            Settings.Instance.Servers.Add(new()
+            {
+                Name = "MainWeb",
+                Port = 80,
+            });
+            Settings.Instance.Servers.Add(new()
+            {
+                Name = "MainWebSSL",
+                Port = 443,
+                UseCerts = true
+            });
+            Settings.Instance.CertDetails.Add(new()
+            {
+                Name = "UbisoftCert",
+                Password = "ServerEmus"
+            });
+            Settings.Instance.CertDetails.Add(new()
+            {
+                Name = "ServerEmusPFX",
+                Password = "ServerEmus"
+            });
+            Settings.Save();
+            return;
+        }
         MainLogger.CreateNew();
         if (args.Contains("clean"))
         {
@@ -19,15 +51,60 @@ internal class Program
         }
         if (args.Contains("debug"))
         {
-            MainLogger.LevelSwitch.MinimumLevel = LogEventLevel.Debug;
+            MainLogger.LevelSwitch.MinimumLevel = LogEventLevel.Verbose;
         }
-        // TODO: More stuff to check and get the cert file.
-        var crt = ModdableWebServer.Helper.CertHelper.GetContextNoValidate(System.Security.Authentication.SslProtocols.Tls12, "cert.pfx", string.Empty);
+        
 
-        ServerController.Start(crt);
+        bool supportSSL = Directory.Exists("Cert");
+
+        if (!supportSSL && Settings.Instance.Servers.Any(x => x.UseCerts = true))
+        {
+            Log.Error("You have not created a 'Cert' folder to include your certificates, but your settings have to user cert. Please make a 'Cert' directory and install any certificate!");
+            return;
+        }
+
+        X509Certificate2Collection Collection = [];
+        foreach (var cert in Directory.GetFiles("Cert"))
+        {
+            if (cert.EndsWith("key"))
+                continue;
+            var certDetail = Settings.Instance.CertDetails.FirstOrDefault(x => cert.Contains(x.Name));
+            string password = string.Empty;
+            if (certDetail != null)
+                password = certDetail.Password;
+            if (cert.EndsWith("pfx"))
+            {
+                Collection.Add(CertHelper.GetCert(cert, password));
+                continue;
+            }
+            var name = Path.GetFileNameWithoutExtension(cert);
+            var keyName = Path.Combine("Cert", $"{name}.key");
+            Collection.Add(CertHelper.GetCertPem(cert, keyName));
+        }
+        SslContext context = new(SslProtocols.Tls12, Collection, CertHelper.NoCertificateValidator);
+
+        List<ServerModel> servers = [];
+
+        foreach (var server in Settings.Instance.Servers)
+        {
+            servers.Add(new()
+            { 
+                Name  = server.Name,
+                Port = server.Port,
+                Context = server.UseCerts ? context : null,
+            });
+        }
+
+        ServerController.Start(servers);
         PluginController.LoadPlugins();
+        List<string> quitList =
+        [
+            "q",
+            "quit",
+            "exit",
+        ];
         string endCheck = "not";
-        while (!endCheck.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+        while (!quitList.Contains(endCheck))
         {
             endCheck = Console.ReadLine()!;
             if (endCheck.StartsWith('!'))
@@ -37,7 +114,7 @@ internal class Program
         }
         PluginController.UnloadPlugins();
         ServerController.Stop();
-        Console.ReadLine();
+        MainLogger.Close();
     }
 
 }
---
+++ ServerApp/Settings.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+
+namespace ServerApp;
+
+internal class Settings
+{
+    private static Settings? instance;
+    private static JsonSerializerOptions serializerOptions = new()
+    {
+        WriteIndented = true,
+        IndentCharacter = '\t',
+        IndentSize = 1,
+    };
+
+    public static Settings Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            if (File.Exists("Settings.json"))
+                instance = JsonSerializer.Deserialize<Settings>(File.ReadAllText("Settings.json"));
+            instance ??= new();
+            File.WriteAllText("Settings.json", JsonSerializer.Serialize(instance, serializerOptions));
+            return instance;
+        }
+    }
+
+    internal static void Save()
+    {
+        File.WriteAllText("Settings.json", JsonSerializer.Serialize(instance, serializerOptions));
+    }
+
+    public class Server
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public bool UseCerts { get; set; }
+    }
+
+    public class CertDetail
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public List<Server> Servers { get; set; } = [];
+
+    public List<CertDetail> CertDetails { get; set; } = [];
+}
